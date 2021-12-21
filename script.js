8bc9fef6180e10eb808259666f8ed0fe1c3cc330
Egor4ek-debug <nabatov.egor@gmail.com>
2021-12-22 01:41:44 +0300

    Production

---
+++ script.js
@@ -0,0 +1,86 @@
+let Data = new Date();
+
+let Hour = Data.getHours();
+
+let div = document.getElementById('Hello');
+
+if (Hour > 3 && Hour<=8){
+    div.innerHTML=`<span style = "color: white; text-transform: uppercase; font-size: 25px">Доброго утра</span>`;
+}
+else if (8<Hour && Hour<=14){
+    div.innerHTML=`<span style = "color: white; text-transform: uppercase; font-size: 25px">Доброго дня</span>`;
+}
+else if (14<Hour && Hour<=18){
+    div.innerHTML=`<span style = "color: white; text-transform: uppercase; font-size: 25px">Добрый вечер</span>`;
+}
+else {
+    div.innerHTML=`<span style = "color: white; text-transform: uppercase; font-size: 25px">Доброй ночи</span>`;
+}
+
+let seconds = 0;
+let minutes  = 0;
+
+function clock(){
+    seconds ++;
+    if (seconds == 60){
+        minutes ++;
+        seconds = 0;
+    }
+    stl = minutes + ':' + seconds;
+    document.getElementById('clock').innerHTML = `<p>Вы на сайте уже</p>`+stl+`<p>минут/секунд</p>`;
+}
+
+setInterval(clock,1000);
+
+function time(){
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds
+    
+    stl=hours + ':' + minutes + ':' + seconds;
+    document.getElementById('time').innerHTML=`<p>Текущее время</p>`+stl;
+    }
+    setInterval(time, 1000);
+
+ 
+    
+let button = document.getElementById('getLocation');
+button.innerHTML = `<button>Узнай свои координаты</button>`
+button.addEventListener('click',()=>{
+    navigator.geolocation.getCurrentPosition(getLocation);
+})
+function getLocation(position) {
+    var latidude=position.coords.latitude;
+    var longitude=position.coords.longitude;
+    document.getElementById('getLocation').innerHTML =`<p>"Широта "</p>` + latidude + `<br>` ;
+    document.getElementById('getLocation').innerHTML =`<p>"Долгота "</p>`+ longitude +`<br>` ;
+    alert("Широта"+latidude+ ' '+ "Долгота"+ longitude)
+    setTimeout(function(){
+        newWin.close();
+    },20000);
+ }
+
+place_play = document.getElementById('play_time');
+place_play.innerHTML = `<button>Хочешь поиграть?</button>`
+
+place_play.addEventListener('click',()=>{
+    alert('Угадай цифру,которую я загадал (от 1 до 5)')
+    max = 5;
+    min = 0;
+    let k = Math.floor(Math.random()* (max - min + 1)+ min);
+    let p = parseInt( prompt("Введите число"));
+    while( p != k){
+        if (p < k){
+            p = parseInt( prompt("Мало взял"));
+        }
+        else{
+            p = parseInt( prompt("Много взял"));
+    }
+ }
+ alert("Да, ты угадал! Загаданным число было " + k)
+})
---
+++ script_sale.js
@@ -0,0 +1,34 @@
+ function timer(){
+    let today = new Date();
+    Hour_today = today.getHours();
+    Minute_today = today.getMinutes();
+    Seconds_today = today.getSeconds();
+    let end_sale = new Date();
+    end_sale.setHours(16, 0, 0);
+    Hour_sale = end_sale.getHours();
+    Minute_sale = end_sale.getMinutes();
+    Seconds_sale = end_sale.getSeconds();
+    let diffetentHours = (Hour_sale) - Hour_today;
+    let differentMinutes = (60-Minute_sale) - Minute_today;
+    let differentSeconds = (60 - Seconds_sale) - Seconds_today;
+    if (diffetentHours<0){
+        diffetentHours=0;
+    }
+    if (diffetentHours<0 && differentMinutes <0){
+        differentMinutes = 0;
+    }
+    if (diffetentHours<0 && differentMinutes <0 && differentSeconds <0 ){
+        differentSeconds = 0;
+    }
+    if (Hour_today<=Hour_sale && Minute_today<=Minute_sale && Seconds_today<=Seconds_sale ){
+        document.getElementById('timer').innerHTML=`<p>Акция подошла к концу</p>`
+        clearInterval(interval_timer);
+    }
+    else {
+
+    document.getElementById('timer').innerHTML=`<p>Часов ${diffetentHours} Минут ${differentMinutes} Секунд ${differentSeconds}</p>`
+    }
+
+ }
+
+ let interval_timer=setInterval(timer,1000);
